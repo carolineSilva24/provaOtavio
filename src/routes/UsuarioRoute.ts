4b52570283b46dd51f4a304789fbc5c3c8e8a466
carolineSilva24 <102956881+carolineSilva24@users.noreply.github.com>
2023-11-27 08:19:21 -0300

    Update UsuarioRoute.ts

--- src/routes/UsuarioRoute.ts
+++ src/routes/UsuarioRoute.ts
@@ -5,12 +5,4 @@ const UsuarioRouter = Router();
 
 UsuarioRouter.get('/users', UsuarioController.listUsuarios);
 
-/*UsuarioRouter.get('/users', UsuarioController.listUsuarios)
-
-UsuarioRouter.post('/user', UsuarioController.createUsuario);
-
-UsuarioRouter.put('/user', UsuarioController.updateUsuarios);
-
-UsuarioRouter.delete('/user', UsuarioController.deleteUsuarios);
-*/
 export default UsuarioRouter;
