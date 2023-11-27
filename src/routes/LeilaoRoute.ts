18f8b1795ce753e219f982ca5d5038b6029921f6
carolineSilva24 <102956881+carolineSilva24@users.noreply.github.com>
2023-11-27 08:19:07 -0300

    Update LeilaoRoute.ts

--- src/routes/LeilaoRoute.ts
+++ src/routes/LeilaoRoute.ts
@@ -5,12 +5,5 @@ import UsuarioRouter from "./UsuarioRoute";
 const LeilaoRouter = Router();
 
 LeilaoRouter.get('/leiloes', LeilaoController.listLeilao);
-/*LeilaoRouter.get('/leiloes', LeilaoController.listLeilao)
 
-LeilaoRouter.post('/leilao', LeilaoController.createLeilao);
-
-LeilaoRouter.put('/leilao', LeilaoController.updateLeilao);
-
-LeilaoRouter.delete('/leilao', LeilaoController.fecharLeilao);
-*/
 export default LeilaoRouter;
